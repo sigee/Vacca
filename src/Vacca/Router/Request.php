a276309bad1812a3b43147977cedde733b3d1b95
sigee <sigee15@gmail.com>
2017-06-09 10:09:31 +0200

    Add a basic logic into the Request to be created
    (cherry picked from commit bbeca8a)

--- src/Vacca/Router/Request.php
+++ src/Vacca/Router/Request.php
@@ -2,8 +2,76 @@
 
 namespace Vacca\Router;
 
-
 class Request
 {
+    /**
+     * @var string
+     */
+    private $uri;
+    /**
+     * @var string
+     */
+    private $method;
+    /**
+     * @var array|false
+     */
+    private $headers;
 
+    function __construct($server = null)
+    {
+        if (is_null($server)) {
+            $server = $_SERVER;
+        }
+        $this->uri = $server['REQUEST_URI'];
+        $this->method = $server['REQUEST_METHOD'];
+        $this->headers = getallheaders();
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @param string $uri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param string $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @return array|false
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param array|false $headers
+     */
+    public function setHeaders($headers)
+    {
+        $this->headers = $headers;
+    }
 }
