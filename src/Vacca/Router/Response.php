b76392d773b20b04a20d54e074f82f080a268fa0
sigee <sigee15@gmail.com>
2017-06-09 10:16:32 +0200

    Add basic logic to the response
    (cherry picked from commit 82ab5c7)

--- src/Vacca/Router/Response.php
+++ src/Vacca/Router/Response.php
@@ -2,12 +2,31 @@
 
 namespace Vacca\Router;
 
-
 class Response
 {
+    /**
+     * @var string
+     */
+    private $body;
+
     public function send()
     {
-
+        echo $this->body;
     }
 
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
 }
