44286beecda6950b4080929378409b651de07c37
sigee <sigee15@gmail.com>
2017-06-09 10:07:48 +0200

    Add routes to the router on get method
    (cherry picked from commit 2066b16)

--- src/Vacca/Router/Router.php
+++ src/Vacca/Router/Router.php
@@ -5,12 +5,17 @@ namespace Vacca\Router;
 class Router
 {
     /**
+     * @var Route[]
+     */
+    private $routes;
+
+    /**
      * @param string $uri
      * @param mixed $mixed
      */
     public function get($uri, $mixed)
     {
-
+        $this->routes[] = new Route($uri, $mixed);
     }
 
     /**
